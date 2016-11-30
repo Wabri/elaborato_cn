576c4c8ed7e215a8c91222b0ce47a07151c8941f
pagano <pagano@debian>
2016-11-30 13:58:05 +0100

    corretti i nomi aggiunto esercizio 1.13

--- cap_1/es6.py
+++ cap_1/es6.py
@@ -1,15 +1,14 @@
-import scipy
 import numpy as np
-import math
 
 x = []
 x.append(2)
 x.append(1.5)
 rad = np.sqrt(2)
-j=0
 
-for i in range (1,15):
-	j=j+1
+print "indice		passo iterativo			delta"
+print "\r"
+
+for i in range (0,16):
 	x.append((x[i]*x[i-1] + 2)/(x[i] +x[i-1]))
-	print str(i) + " " + str(x[i]) + " " + str(x[i]-rad)
+	print str(i) + "		" + str(x[i]) + "		" + str(x[i]-rad)
 
--- cap_1/esercizio1_6.py
+++
@@ -1,4 +0,0 @@
-import scipy,numpy,math
-x = []
-print x
-x[0]=2
