1f7964202cf71c52f193beaf60335d111ca8b237
Wabri <gabriele.puliti@gmail.com>
2017-03-24 17:10:38 +0100

    create example for es5 in es14, es6 es11 es12 left

--- cap_3/LUP.m
+++ cap_3/LUP.m
@@ -2,12 +2,12 @@
 %Esercizio 5 capitolo 3
 %Fattorizzazione LU con pivoting parziale
 
-function [M, p] =  LUP(A)
+function [A, p] =  LUP(A)
     [m,n]=size(A);
     if m~=n
         error('Errore: matrice non quadrata');
     end
-    p=1:n;
+    p=[1:n];
     for i=1:(n-1)
         [aki, ki] = max(abs(A(i:n, i)));
         if aki==0
@@ -18,8 +18,7 @@ function [M, p] =  LUP(A)
             A([i, ki], :) = A([ki, i], :);
             p([i, ki]) = p([ki, i]);
         end
-        A(i+1:n, i) = A(i+1:n, i)/A(i, i);
-        A(i+1:n, i+1:n) = A(i+1:n, i+1:n) -A(i+1:n, i)*A(i,i+1:n);
+        A((i+1):n, i) = A((i+1):n, i)/A(i, i);
+        A((i+1):n, i+1:n) = A((i+1):n, (i+1):n) -A((i+1):n, i)*A(i,(i+1):n);
     end
-    M = A;
 end
---
+++ cap_3/es14.m
@@ -0,0 +1,6 @@
+% esempio es5
+A=[0,-3,8;-1,8,7;1,3,0]
+[LU,p] = LUP(A)
+% esempio es6
+% esempio es11
+% esempio es12
