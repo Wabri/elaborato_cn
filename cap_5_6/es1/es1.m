9f9b2a9d2668f0a06cdaebaf67c59cb0c6475899
pagano <pagano@debian>
2017-06-18 15:52:46 +0200

    fixed gausseidel and jacobi

--- cap_5_6/es1/es1.m
+++
@@ -1,9 +0,0 @@
-% 	ATTENZIONE !!!
-%	NON TESTATO
-%	DVX NOBIS
-
-In = trapeziComposita(f,a,b,n);
-disp(In);
-In = simpsonComposita(f,a,b,n);
-disp(In);
-    
---
+++ cap_5_6/gaussSeidel.m
@@ -0,0 +1,22 @@
+function [x1, k] = gaussSeidel(A, b, x0, nmax, tol)
+    D=diag(diag(A));
+    L=tril(A)-D; 
+    U=triu(A)-D;
+    DI=inv(D+L); 
+    GS=-DI*U;
+    b1=(D+L)\b; 
+    x1=GS*x0+b1; 
+    k=1;
+    err(k)=norm(x1-x0,inf)/norm(x1);
+
+    while(err(k)>tol && k<=nmax)
+	x0=x1;
+	x1=GS*x0+b1;
+	k=k+1;
+	err(k)=norm(x1-x0,inf)/norm(x1);
+    end
+    if k>nmax
+	disp('** Gauss-Seidel non converge nel numero di iterazioni fissato');
+    end
+    k=k-1;
+end
