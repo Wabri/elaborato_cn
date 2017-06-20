51b36ddd8e0bf8fb65b1c5d31de7a294951e7919
Gabriele Gemmi <gabriel@autistici.org>
2017-06-20 15:08:26 +0200

    e lafine invece no

--- cap_5_6/es6/es6.m
+++ cap_5_6/es6/es6.m
@@ -1,32 +1,7 @@
-function [x] = PotenzePR(H, p, tol)
-  [m,n] = size(H);
-  if m~=n
-    error('Errore: H non quadrata');
-  end
-  delta = zeros(n, 1);
-  for j=1:n
-    for i=1:n
-      delta(j) = delta(j) + H(i, j);
-    end
-  end
-  for j=1:n
-    for i=1:n
-      if delta(j)==0
-        H(i, j) = 0;
-      else
-        H(i, j) = H(i, j)/delta(j);
-      end
-    end
-    if delta(j)==0
-      delta(j)=1;
-    else
-      delta(j)=0;
-    end
-  end
-  x = rand(n,1);
-  x = x/norm(x);
-  imax = (log(tol)-log(2))/(log(p));
-  for i=1:imax
-    x = p*(H*x)+((1+p*(delta'*x -1))/n)*ones(n,1);
-  end
-end
+S = [0,0,0,1/5,1/5;1/2,0,1,1/5,1/5;1/2,1/3,0,1/5,1/5;0,1/3,0,1/5,1/5;0,1/3,0,1/5,1/5]
+n=5;
+p=0.85;
+A=eye(n) - p*S;
+b = ((1-p)/n).*ones(n,1);
+
+C= p.*S + b;
