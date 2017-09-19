88db4f8c76c2aad3da4fd1ab156f0432872529e4
Wabri <gabriele.puliti@gmail.com>
2017-09-19 13:59:20 +0200

    cap2_1 some refactor and adding to es1

--- cap_1/es13/es13.m
+++ cap_1/es13/es13.m
@@ -8,4 +8,9 @@ for i = 1:1001
 end
 
 plot(x,y);
+xlabel('x');
+ylabel('f(x)');
+disp ('valore interno al limite in x=4/3 : ');
 disp (abs(3*(1-4/3)+1))
+disp ('la funzione calcolata in 4/3 ha in output:');
+disp(log(abs(3*(1-(4/3))+1))/80 + (4/3)^2 +1);
