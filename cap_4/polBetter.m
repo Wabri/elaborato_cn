bfb12b8d9e1b539cdd0b01f0bdc04e53b8dce5ab
Wabri <gabriele.puliti@gmail.com>
2017-10-03 18:55:18 +0200

    review cap4_8, es 4 5 6 left

---
+++ cap_4/polBetter.m
@@ -0,0 +1,20 @@
+% m -> grado del polinomio
+function [y] = polBetter(x,y, m)
+  x=x';
+  
+  %controllo esistenza di m+1 ascisse distinte
+  if length(unique (x)) < m+1
+    error('[Errore] Non ci sono m ascisse distinte');
+  end
+  
+  %creazione matrice vandermonde rettangolare
+  V(:,m+1) = ones(length(x),1);
+  for j = m:-1:1
+    V(:,j) = x.*V(:,j+1);
+  end
+  V
+ 
+  %backslash
+  y = V\y';
+  y=y';
+end
